a6e8165c5196af94fd700e8b2a037332ccf5a538
Ranjitha H S <128285640+HSRanjitha2003@users.noreply.github.com>
2024-02-09 20:24:56 +0530

    Update file.php

--- file.php
+++ file.php
@@ -1,6 +1,6 @@
 <html lang = "en">
   <head>
-    <title> Process the popcorn3.html form </title>
+    <title> Rental House System</title>
 
     <meta charset = "utf-8" />
     <style type = "text/css">
