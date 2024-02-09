323e793fcb609a73983bc40f5e87d3207975377a
Ranjitha H S <128285640+HSRanjitha2003@users.noreply.github.com>
2024-02-09 20:23:59 +0530

    Update file.php

--- file.php
+++ file.php
@@ -1,8 +1,38 @@
+<html lang = "en">
+  <head>
+    <title> Process the popcorn3.html form </title>
+
+    <meta charset = "utf-8" />
+    <style type = "text/css">
+      td, th, table {border: thin solid black;}
+    </style>
+  </head>
+  <body>
+
 <?php
-echo 
-print("Hello World!");
-$a=10;
-
-print($a);
-
- ?>
+$name = $_POST['name']';
+$phone = $_POST['phone'];
+$description= $_POST['description'];
+$question = $_POST['question'];
+$question = $_POST['question'];
+?>
+<table>
+      <caption> Renter Information </caption>
+ <tr>
+        <th> Name</th>
+        <th> Phone Number</th>
+        <th> Description </th>
+        <th> Question </th>
+      </tr>
+     <tr>
+     <td> <?php printf ("$ %s", $name); ?>
+        </td>
+        <td> <?php printf ("$ %s", $phone); ?>
+        </td><td> <?php printf ("$ %s", $description); ?>
+        </td><td> <?php printf ("$ %s", $question); ?>
+        </td>
+        </tr>
+    </table>
+    </body>
+    </html>
+  
