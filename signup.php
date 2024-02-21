1a95faf81965f00e33a45c7c312812acd8c2d5b4
Ranjitha H S <128285640+HSRanjitha2003@users.noreply.github.com>
2024-02-21 12:05:16 +0530

    Create signup.php

---
+++ signup.php
@@ -0,0 +1,36 @@
+<?php
+
+// Database connection
+$servername = "localhost";
+$username = "root"; // Replace with your MySQL username
+$password = ""; // Replace with your MySQL password
+$dbname = "flatdatabase";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+// Check connection
+if ($conn->connect_error) {
+    die("Connection failed: " . $conn->connect_error);
+}
+
+// Retrieve form data
+$email = $_POST['email'];
+$username=$_POST['username'];
+$password = $_POST['password'];
+
+
+
+// Prepare SQL statement
+$sql = "INSERT INTO signup (email,username,password)
+        VALUES ('$email','$username', '$password')";
+
+// Execute SQL statement
+if ($conn->query($sql) === TRUE) {
+    echo "Flat renter information is succesfully stored";
+} else {
+    echo "Error: " . $sql . "<br>" . $conn->error;
+}
+
+$conn->close();
+
+?>
